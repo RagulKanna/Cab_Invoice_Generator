7fea5259653900d12a2dbd5c83556b3202d61f8c
RagulKanna <ragulragi619@gmail.com>
2021-09-28 13:04:15 +0530

    Added:Calculate the fare for multiple rides by total_fare

--- src/main/java/com/InvoiceGenerating/InvoiceGenerator.java
+++ src/main/java/com/InvoiceGenerating/InvoiceGenerator.java
@@ -7,9 +7,18 @@ public class InvoiceGenerator {
 
     public double calculateFare(double distance, int time) {
 
-        double totalFare = distance*MINIMUM_COST_PER_KILOMETER + time*COST_PER_TIME;
-        if(totalFare<MINIMUM_FARE)
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE)
             return MINIMUM_FARE;
+        return totalFare;
+    }
+
+    public double calculateFare(Rides[] rides) {
+
+        double totalFare = 0;
+        for (Rides ride : rides) {
+            totalFare = totalFare + this.calculateFare(ride.distance, ride.time);
+        }
         return totalFare;
     }
 }
