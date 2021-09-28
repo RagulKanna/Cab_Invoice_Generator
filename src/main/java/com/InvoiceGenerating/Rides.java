3dd9205ba70773d08033cb20a9394f84d9d70f4c
RagulKanna <ragulragi619@gmail.com>
2021-09-28 14:07:50 +0530

    Added:Invoice generating for premium user

--- src/main/java/com/InvoiceGenerating/Rides.java
+++ src/main/java/com/InvoiceGenerating/Rides.java
@@ -3,9 +3,11 @@ package com.InvoiceGenerating;
 public class Rides {
     public double distance;
     public int time;
+    public String user;
 
-    public Rides(double distance, int time) {
+    public Rides(double distance, int time, String user) {
         this.distance = distance;
         this.time = time;
+        this.user = user;
     }
 }
